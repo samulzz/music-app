7fb226bc08101c87b682cc6277d7a9109c43cb1c
Samuel Araujo Brota <samuel.araujo.130823@gmail.com>
2026-03-13 16:04:30 -0300

    added react-native-track-player and services

---
+++ app-front/nationmusics/services/playerSetup.ts
@@ -0,0 +1,35 @@
+import TrackPlayer, { Capability, AppKilledPlaybackBehavior } from 'react-native-track-player';
+
+export async function setupPlayer() {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getCurrentTrack();
+    isSetup = true;
+  } catch {
+    await TrackPlayer.setupPlayer();
+
+    await TrackPlayer.updateOptions({
+      android: {
+        appKilledPlaybackBehavior: AppKilledPlaybackBehavior.ContinuePlayback,
+      },
+
+      capabilities: [
+        Capability.Play,
+        Capability.Pause,
+        Capability.SkipToNext,
+        Capability.SkipToPrevious,
+        Capability.SeekTo,
+      ],
+      compactCapabilities: [
+        Capability.Play,
+        Capability.Pause,
+        Capability.SkipToNext,
+      ],
+      progressUpdateEventInterval: 2,
+    });
+
+    isSetup = true;
+  } finally {
+    return isSetup;
+  }
+}

b2bef19902cacaa79daa488a66079dbdad2d8d09
Samuel Araujo Brota <samuel.araujo.130823@gmail.com>
2026-03-14 11:33:10 -0300

    Update MusicApiApplication.java

--- api-restfull/music-api/src/main/java/me/samulsz/musicapi/MusicApiApplication.java
+++ api-restfull/music-api/src/main/java/me/samulsz/musicapi/MusicApiApplication.java
@@ -14,24 +14,6 @@ public class MusicApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicApiApplication.class, args);
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.ipify.org"))
-                    .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String ipPublico = response.body();
-
-            System.out.println("\n=======================================================");
-            System.out.println("🚀 SERVIDOR SOULSEEKER ONLINE!");
-            System.out.println("🌐 SEU IP PÚBLICO É: " + ipPublico);
-            System.out.println("🔗 TESTE NO NAVEGADOR: http://" + ipPublico + ":8080/api/musicas/status");
-            System.out.println("=======================================================\n");
-
-        } catch (Exception e) {
-            System.out.println("Servidor online, mas não foi possível descobrir o IP Público externo.");
-        }
     }
 
 }
